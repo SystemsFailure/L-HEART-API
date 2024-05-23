1762242effe7dfc481c1cd9d3ac4a99749549b23
SystemsFailure <allistirking422@gmail.com>
2024-05-23 21:01:05 +0300

    update account module

--- app/modules/account/account.controller.ts
+++ app/modules/account/account.controller.ts
@@ -5,18 +5,52 @@ import { createAccountValidator } from '#validators/create_account';
 import Account from '#models/account';
 import User from '#models/user';
 import { AccessToken } from '@adonisjs/auth/access_tokens';
+import { ModelPaginatorContract } from '@adonisjs/lucid/types/model';
 
 
 @inject()
 export default class AccountController {
   constructor(private userService: AccountService) {}
 
-  async index({  }: HttpContext) {
+  async index({ request, response }: HttpContext) {
+    try {
+      // Получаем параметры запроса с помощью деструктуризации и задаем значения по умолчанию
+      const { perPage = 20, currentPage = 1 } = request.only(['perPage', 'currentPage']);
+      const { search, filter } = request.only(['search', 'filter'])
 
+      // Создаем базовый запрос
+      const query = Account.query()
+        .select('id', 'email', 'name', 'created_at', 'updated_at')
+        .preload('profiles');
+
+      // Применяем фильтры и поиск, если они заданы
+      if (search) {
+        query.where('name', 'LIKE', `%${search}%`)
+            .orWhere('email', 'LIKE', `%${search}%`);
+      }
+
+      if (filter) {
+        Object.entries(filter).forEach(([key, value]) => {
+          query.where(key, String(value));
+        });
+      }
+
+      // Пагинация
+      const result: ModelPaginatorContract<Account> = await query.paginate(currentPage, perPage);
+      return response.apiSuccess(result);
+    } catch (error) {
+      console.error(error)
+    }
   }
 
-  async show({  }: HttpContext) {
+  async show({ response, params }: HttpContext) {
+    const result: Account | null = await Account.find(params.id)
 
+    if(!result) {
+      return response.apiError({}, `По данному Id=${params.id} аккаунт не найден`)
+    }
+
+    return response.apiSuccess(result)
   }
 
   async create({ request, response }: HttpContext) {
@@ -36,10 +70,11 @@ export default class AccountController {
         password: payload.password,
     })
     
+    await account.save()
+
     const user: User = new User();
     user.account_id = account.id
 
-    await account.save()
     await user.save()
     const token: AccessToken = await Account.accessTokens.create(account)
 
